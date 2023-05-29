2859992cae7f30d952c926cba0e6ee371abfa1e8
santii90ec <95385224+santii90ec@users.noreply.github.com>
2023-05-29 11:19:36 -0500

    Add files via upload

---
+++ ToDoistApp/core/src/models/task.py
@@ -0,0 +1,4 @@
+class Task:
+    def __init__(self, id: str, description: str) -> None:
+        self.id = id
+        self.description = description
---
+++ ToDoistApp/core/src/use_cases/add_task.py
@@ -0,0 +1,5 @@
+import models.task
+
+
+class AddTask:
+    pass
